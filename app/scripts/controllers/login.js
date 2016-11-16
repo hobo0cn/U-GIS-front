f21bd25a05153cb29be293c4d6f4670362ef42e1
hobo0cn <hobo0cn@gmail.com>
2016-11-16 15:05:19 +0800

    fix grunt build bug.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -5,24 +5,25 @@ angular.module('uGisFrontApp')
     'ProfileServices',  'ProfileInfoService',
    function ($scope, $location, $cookies, TokenAuthService, 
     ProfileServices, ProfileInfoService) {
-   	$scope.username = "";
-   	$scope.password = "";
+   	$scope.username = '';
+   	$scope.password = '';
     $scope.error_username_or_pwd = 0;
     $scope.isAutoLogin =  $cookies.get('EDM_isAutoLogin');
 
 
     var _routeByUserCategory = function(usercat){
-      if (usercat == 'A') {
+      if (usercat.localeCompare('A') === 0) {
         $location.path('/project');
       }
-      else if (usercat == 'P' ) {
+      else if (usercat.localeCompare('P') === 0 ) {
         $location.path('/task');
       }
-      else if(usercat == 'S'){
+      else if(usercat.localeCompare('S') === 0){
         $location.path('/datadashboard');
       }
-      else if (usercat == 'O') {
+      else if (usercat.localeCompare('O') === 0) {
         $location.path('/dashboard');
+
       }
     };
 
@@ -38,12 +39,12 @@ angular.module('uGisFrontApp')
                 $cookies.put('EDM_usercat',   response.user_cat);
                 $cookies.put('EDM_userid',   response.id);
                 //判断用户类型，跳转到不同的页面
-                _routeByUserCategory(response.user_cat)
+                _routeByUserCategory(response.user_cat);
                      
             },
             function error(errorResponse){
                 console.log('Error:' + JSON.stringify(errorResponse));
-                if (errorResponse.status == 400) {
+                if (errorResponse.status === 400) {
                   //错误的用户名或密码
                    $scope.error_username_or_pwd = 1;
                 }
@@ -57,7 +58,7 @@ angular.module('uGisFrontApp')
 
     $scope.isAutoLogin =  $cookies.get('EDM_isAutoLogin');
     //如果是自动登录，则直接调用缓存中的username和token进行验证
-    if ($scope.isAutoLogin == 'true' ) {
+    if ($scope.isAutoLogin === 'true' ) {
       ProfileInfoService.get({userid: $cookies.get('EDM_userid')}, 
             function success(response){
               console.log('Success:' + JSON.stringify(response));
@@ -66,12 +67,12 @@ angular.module('uGisFrontApp')
                 $cookies.put('EDM_usercat',   response.user_cate);
                 $cookies.put('EDM_userid',   response.id);
                 //判断用户类型，跳转到不同的页面
-                _routeByUserCategory(response.user_cate)
+                _routeByUserCategory(response.user_cate);
                      
             },
             function error(errorResponse){
                 console.log('Error:' + JSON.stringify(errorResponse));
-                if (errorResponse.status == 401) {
+                if (errorResponse.status === 401) {
                   //错误的用户名或密码
                    $scope.error_username_or_pwd = 1;
                 }
